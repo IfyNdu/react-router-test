4c3146125a7ba047d6787105de1a947f1457f9c1
Ify Ndu <ify@ifys-mbp.home>
2019-10-28 23:21:17 +0000

    React router test with history api

--- src/views/app/app.js
+++ src/views/app/app.js
@@ -1,13 +1,60 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
 
 import styles from './app.scss';
 
+const Home = () => {
+  return <h2>Home</h2>;
+};
+
+const About = () => {
+  return <h2>About</h2>;
+};
+
+const Users = () => {
+  return <h2>Users</h2>;
+};
 
 class App extends React.Component {
   render() {
     return (
       <div className={styles.root}>
-        BOILER PLATE
+        <div className={styles.header}>
+          <h1>Route Me</h1>
+        </div>
+        <Router>
+          <div>
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/about">About</Link>
+                </li>
+                <li>
+                  <Link to="/users">Users</Link>
+                </li>
+              </ul>
+            </nav>
+            <Switch>
+              <Route path="/about">
+                <About />
+              </Route>
+              <Route path="/users">
+                <Users />
+              </Route>
+              <Route path="/">
+                <Home />
+              </Route>
+            </Switch>
+          </div>
+        </Router>
       </div>
     );
   }
